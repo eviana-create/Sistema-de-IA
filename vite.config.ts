1155c3de8efb89ed613d71047ea7ab2eb2008fd4
Emerson Viana <missonzito@outlook.com>
2025-08-06 00:45:01 -0300

    chore: config Vite para publicação no GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+// Substitua "nome-do-repo" pelo nome exato do seu repositório no GitHub
+const repo = "ai-wa-stylist"; // ou outro nome exato do seu repositório
+
 export default defineConfig({
+  base: `/${repo}/`, // necessário para funcionar corretamente no GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
